04d10ca3a5592a82e4fa75e3e10449cbd82ef662
Thuotracy <tracyjacobs775@gmail.com>
2022-07-04 17:39:51 +0300

    created variables in main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+var start =  document.getElementById('start');
+var reset = document.getElementById('reset');
+var stop = document.getElementById('stop');
+
+
+var wm = document.getElementById('w_minutes');
+var ws = document.getElementById('w_seconds');
+
+var bm = document.getElementById('b_minutes');
+var bs = document.getElementById('b_seconds');
